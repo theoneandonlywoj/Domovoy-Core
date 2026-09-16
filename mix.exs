99797e87a19d3241a09410e14e8ce6dc735a4d6c
theoneandonlywoj <theoneandonlywoj@gmail.com>
2026-09-16 21:46:08 +0200

    chore(project): update for prototype engine structure

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule DomovoyCore.MixProject do
       version: "0.1.0",
       elixir: "~> 1.20",
       start_permanent: Mix.env() == :prod,
+      elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps(),
       aliases: aliases(),
       dialyzer: [
@@ -24,17 +25,23 @@ defmodule DomovoyCore.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:crypto, :logger]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ecto, "~> 3.12"},
+      {:phoenix_pubsub, "~> 2.1"},
+      {:telemetry, "~> 1.0"},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
     ]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_env), do: ["lib"]
 
   defp aliases do
     [
