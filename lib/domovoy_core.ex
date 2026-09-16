55c30cb01ef3684d9d12ace5d5c139025e01acad
theoneandonlywoj <theoneandonlywoj@gmail.com>
2026-09-16 20:22:05 +0200

    chore(quality): add local quality checks

--- lib/domovoy_core.ex
+++ lib/domovoy_core.ex
@@ -12,6 +12,7 @@ defmodule DomovoyCore do
       :world
 
   """
+  @spec hello() :: :world
   def hello do
     :world
   end
--- mix.exs
+++ mix.exs
@@ -5,9 +5,19 @@ defmodule DomovoyCore.MixProject do
     [
       app: :domovoy_core,
       version: "0.1.0",
-      elixir: "~> 1.19",
+      elixir: "~> 1.20",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases(),
+      dialyzer: [
+        plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+      ]
+    ]
+  end
+
+  def cli do
+    [
+      preferred_envs: [precommit: :test]
     ]
   end
 
@@ -21,8 +31,25 @@ defmodule DomovoyCore.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp aliases do
+    [
+      precommit: [
+        "compile --warnings-as-errors",
+        "deps.unlock --unused",
+        "format",
+        "test"
+      ],
+      quality: [
+        "compile --warnings-as-errors",
+        "format --check-formatted",
+        "credo --strict",
+        "dialyzer"
+      ]
     ]
   end
 end
